0df1c8b2943d7efa3e5c5f65e0a4143509225eec
Josh-Darling13 <squiddarling@gmail.com>
2023-07-12 21:01:59 -0400

    function complete

--- dna.py
+++ dna.py
@@ -1,1 +1,10 @@
+"""
+When I said a really simple function, I meant REALLY simple.
+    """
+
+def dna_to_rna(dnaCode):
+    to_re = dnaCode.upper()
+    return to_re.replace("T","U")
+
+print(dna_to_rna("gactttagcttcgatgatcgatgtgtcagagtgtcagaa"))
 
